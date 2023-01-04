0b1d50510e4209dc35cf8d13f34be60f931c3fa3
Christopher Woodall <woodall.christopher@gmail.com>
2023-01-04 16:21:43 -0500

    Update model.py

--- storyteller/model.py
+++ storyteller/model.py
@@ -7,7 +7,7 @@ from TTS.api import TTS
 from diffusers import StableDiffusionPipeline
 import nltk
 from nltk.tokenize import sent_tokenize
-import soundfile as sf
+import soundfile
 import torch
 from transformers import pipeline
 
@@ -39,42 +39,51 @@ class StoryTeller:
         self.painter = StableDiffusionPipeline.from_pretrained(
             self.config.painter,
             torch_dtype=torch.float16,
-            revision="fp16",
+            # revision="fp16",
             use_auth_token=False,
         ).to(painter_device)
         if self.config.disable_nsfw_check:
             self.painter.safety_checker = self.safety_checker
-        self.speaker = TTS(config.speaker)
+        self.speaker = TTS(config.speaker, progress_bar=True, gpu=True)
         self.sample_rate = self.speaker.synthesizer.output_sample_rate
+
 
     @classmethod
     def from_default(cls):
         config = StoryTellerConfig()
         return cls(config)
 
+
     @classmethod
     def from_config(cls, config: StoryTellerConfig):
         return cls(config)
+
 
     @classmethod
     def safety_checker(cls, images, **kwargs):
         return images, False
 
+
     @torch.inference_mode()
     def paint(self, prompt) -> Image:
-        return self.painter(f"{self.config.diffusion_prompt_prefix}: {prompt}").images[
-            0
-        ]
+        return self.painter(
+            f"{self.config.painter_prompt_prefix} {prompt}, {self.config.painter_prompt_postfix}"
+        ).images[0]
+
 
     @torch.inference_mode()
     def speak(self, prompt) -> List[int]:
         return self.speaker.tts(prompt)
 
+
     @torch.inference_mode()
     def write(self, prompt) -> str:
-        return self.writer(prompt, max_new_tokens=self.config.max_new_tokens)[0][
-            "generated_text"
-        ]
+        return self.writer(
+            prompt,
+            max_new_tokens=self.config.max_new_tokens,
+            prefix=self.config.writer_prefix,
+        )[0]["generated_text"]
+
 
     def generate(
         self,
@@ -88,7 +97,13 @@ class StoryTeller:
             video_paths.append(video_path)
         self.concat_videos(video_paths)
 
-    def concat_videos(self, video_paths: List[str]) -> None:
+
+    def concat_videos(self, video_paths: List[dict]) -> None:
+        for video in video_paths:
+            subprocess_run(
+                f"ffmpeg -loop 1 -i {video.image_path} -i {video.audio_path} -vf subtitles={video.subtitle_path} -tune stillimage -shortest {video.video_path}"
+            )
+
         files_path = os.path.join(self.config.output_dir, "files.txt")
         output_path = os.path.join(self.config.output_dir, "out.mp4")
         with open(files_path, "w+") as f:
@@ -96,7 +111,8 @@ class StoryTeller:
                 f.write(f"file {os.path.split(video_path)[-1]}\n")
         subprocess_run(f"ffmpeg -f concat -i {files_path} -c copy {output_path}")
 
-    def _generate(self, id_: int, sentence: str) -> str:
+
+    def _generate(self, id_: int, sentence: str) -> dict:
         image_path = os.path.join(self.config.output_dir, f"{id_}.png")
         audio_path = os.path.join(self.config.output_dir, f"{id_}.wav")
         subtitle_path = os.path.join(self.config.output_dir, f"{id_}.srt")
@@ -108,17 +124,25 @@ class StoryTeller:
         if remainder:
             duration += 1
             audio.extend([0] * (self.sample_rate - remainder))
-        sf.write(audio_path, audio, self.sample_rate)
+        soundfile.write(audio_path, audio, self.sample_rate)
         subtitle = f"0\n{make_timeline_string(0, duration)}\n{sentence}"
         with open(subtitle_path, "w+") as f:
             f.write(subtitle)
-        subprocess_run(
-            f"ffmpeg -loop 1 -i {image_path} -i {audio_path} -vf subtitles={subtitle_path} -tune stillimage -shortest {video_path}"
-        )
-        return video_path
+        return {
+            "subtitle": subtitle_path,
+            "audio": audio_path,
+            "image": image_path,
+            "video": video_path
+        }
+
+
+    def generate_audio(self, sentence: str) -> None:
+        ...
+
 
     def write_story(self, prompt: str, num_sentences: int) -> List[str]:
-        sentences = []
+        # sentences = []
+        sentences = sent_tokenize(prompt)
         while len(sentences) < num_sentences + 1:
             prompt = self.write(prompt)
             sentences = sent_tokenize(prompt)
