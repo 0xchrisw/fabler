a223774ea0c6363a1b8b7d5786e9925aaa8740b9
Christopher Woodall <woodall.christopher@gmail.com>
2023-01-05 14:25:11 -0500

    separate pipelines

--- storyteller/model.py
+++ storyteller/model.py
@@ -6,16 +6,18 @@ from typing import List
 from PIL.Image import Image
 from TTS.api import TTS
 from diffusers import StableDiffusionPipeline
-import nltk
-from nltk.tokenize import sent_tokenize
+# import nltk
+# from nltk.tokenize import sent_tokenize
 import soundfile
 import torch
 from transformers import pipeline
 
 from storyteller import StoryTellerConfig
+from storyteller.pipelines import speaker
+from storyteller.pipelines import writer
 from storyteller.utils import (
     check_ffmpeg,
-    make_timeline_string,
+    # make_timeline_string,
     set_seed,
     subprocess_run,
 )
@@ -28,33 +30,25 @@ logging.getLogger("transformers").setLevel(logging.CRITICAL)
 class StoryTeller:
     def __init__(self, config: StoryTellerConfig):
         check_ffmpeg()
-        nltk.download("punkt")
         set_seed(config.seed)
         self.config = config
         os.makedirs(config.output_dir, exist_ok=True)
-        writer_device = torch.device(config.writer_device)
         painter_device = torch.device(config.writer_device)
-        self.writer = pipeline(
-            "text-generation", model=config.writer, device=writer_device
-        )
-        self.painter = StableDiffusionPipeline.from_pretrained(
-            self.config.painter,
-            torch_dtype=torch.float16,
-            # revision="fp16",
-            use_auth_token=False,
-        ).to(painter_device)
-        if not self.config.nsfw_check:
-            self.painter.safety_checker = self.safety_checker
-        self.speaker = TTS(config.speaker, progress_bar=True, gpu=True)
-        self.sample_rate = self.speaker.synthesizer.output_sample_rate
+        self.writer = writer.init(self.config)
+        # self.speaker = TTS(config.speaker, progress_bar=True, gpu=True)
+        # self.sample_rate = self.speaker.synthesizer.output_sample_rate
+        self.speaker = speaker.init(self.config)
+        # self.painter = StableDiffusionPipeline.from_pretrained(
+        #     self.config.painter,
+        #     torch_dtype=torch.float16,
+        #     # revision="fp16",
+        #     use_auth_token=False,
+        # ).to(painter_device)
+        # if not self.config.nsfw_check:
+        #     self.painter.safety_checker = self.safety_checker
 
     @classmethod
-    def from_default(cls):
-        config = StoryTellerConfig()
-        return cls(config)
-
-    @classmethod
-    def from_config(cls, config: StoryTellerConfig):
+    def init(cls, config: StoryTellerConfig = StoryTellerConfig()):
         return cls(config)
 
     @classmethod
@@ -67,17 +61,9 @@ class StoryTeller:
             f"{self.config.painter_prompt_prefix} {prompt}, {self.config.painter_prompt_postfix}"
         ).images[0]
 
-    @torch.inference_mode()
-    def speak(self, prompt) -> List[int]:
-        return self.speaker.tts(prompt)
-
-    @torch.inference_mode()
-    def write(self, prompt) -> str:
-        return self.writer(
-            prompt,
-            max_new_tokens=self.config.max_new_tokens,
-            prefix=self.config.writer_prompt_prefix,
-        )[0]["generated_text"]
+    # @torch.inference_mode()
+    # def speak(self, prompt) -> List[int]:
+    #     return self.speaker.tts(prompt)
 
     def generate(
         self,
@@ -85,11 +71,35 @@ class StoryTeller:
         num_images: int,
     ) -> None:
         video_paths = []
-        sentences = self.generate_text(prompt, num_images)
+
+        # If there is an even number of sentences, use self.generate_text()
+        # to generate a final sentence; useful for animations
+        if num_images % 2 == 0:
+            num_images += 1
+        # sentences = self.writer.generate(prompt, num_images)
+        sentences = [
+            "Good evening, and welcome to the nightly news. It is March 9th 8163.",
+            "In a shocking turn of events, a toaster has been appointed as the CEO of the largest technology company in the world.",
+            "The announcement has caused widespread concern and debate about the role of artificial intelligence in the workplace.",
+            "According to sources, the toaster was chosen for its efficiency and attention to detail."
+            "... as well as its ability to handle multiple tasks at once.",
+            "Critics argue that the appointment sets a dangerous precedent and could lead to the replacement of human workers with machines.",
+            "The toaster, who declined to be interviewed, has already implemented several changes at the company.",
+            "The changes include the installation of additional outlets in the office for toasting bread and the introduction of a toaster-themed dress code.",
+            "Employees at the company have mixed reactions to the appointment.",
+        ]
         for i, sentence in enumerate(sentences):
             video_path = self._generate(i, sentence)
             video_paths.append(video_path)
-        self.concat_videos(video_paths)
+        # self.concat_videos(video_paths)
+
+    def _generate(self, id_: int, sentence: str) -> dict:
+        return {
+            "audio": self.speaker.generate(id_, sentence),
+            "image": self.generate_image(id_, sentence),
+            "subtitle": Path(f"{self.config.output_dir}/{id_}.srt"),
+            "video": Path(f"{self.config.output_dir}/{id_}.mp4"),
+        }
 
     def concat_videos(self, video_paths: List[dict]) -> None:
         files_data = []
@@ -104,42 +114,33 @@ class StoryTeller:
         files_path.write_text("\n".join(files_data))
         subprocess_run(f"ffmpeg -f concat -i {files_path} -c copy {output_path}")
 
-    def _generate(self, id_: int, sentence: str) -> dict:
-        return {
-            "audio": self.generate_audio(id_, sentence),
-            "image": self.generate_image(id_, sentence),
-            "subtitle": Path(f"{self.config.output_dir}/{id_}.srt"),
-            "video": Path(f"{self.config.output_dir}/{id_}.mp4"),
-        }
+    # def generate_audio(self, id_: int, sentence: str) -> str:
+    #     audio_path = os.path.join(self.config.output_dir, f"{id_}.wav")
+    #     subtitle_path = os.path.join(self.config.output_dir, f"{id_}.srt")
+    #     audio = self.speak(sentence)
+    #     duration, remainder = divmod(len(audio), self.sample_rate)
+    #     if remainder:
+    #         duration += 1
+    #         audio.extend([0] * (self.sample_rate - remainder))
+    #     soundfile.write(audio_path, audio, self.sample_rate)
+    #     subtitle = f"0\n{make_timeline_string(0, duration)}\n{sentence}"
+    #     with open(subtitle_path, "w+") as f:
+    #         f.write(subtitle)
+    #     return audio_path
 
-    def generate_text(
-        self, prompt: str, num_sentences: int, skip: bool = False
-    ) -> List[str]:
-        sentences = sent_tokenize(prompt)
-        story_length = num_sentences + len(sentences)
-        while len(sentences) < story_length:
-            prompt = self.write(prompt)
-            sentences = sent_tokenize(prompt)
-        while len(sentences) > num_sentences:
-            sentences.pop()
-        return sentences
-
-    def generate_audio(self, id_: int, sentence: str, skip: bool = False) -> str:
-        audio_path = os.path.join(self.config.output_dir, f"{id_}.wav")
-        subtitle_path = os.path.join(self.config.output_dir, f"{id_}.srt")
-        audio = self.speak(sentence)
-        duration, remainder = divmod(len(audio), self.sample_rate)
-        if remainder:
-            duration += 1
-            audio.extend([0] * (self.sample_rate - remainder))
-        soundfile.write(audio_path, audio, self.sample_rate)
-        subtitle = f"0\n{make_timeline_string(0, duration)}\n{sentence}"
-        with open(subtitle_path, "w+") as f:
-            f.write(subtitle)
-        return audio_path
-
-    def generate_image(self, id_: int, sentence: str, skip: bool = False) -> str:
+    def generate_image(self, id_: int, sentence: str) -> str:
         image_path = os.path.join(self.config.output_dir, f"{id_}.png")
-        image = self.paint(sentence)
-        image.save(image_path)
+        # image = self.paint(sentence)
+        # image.save(image_path)
         return image_path
+
+    def generate_animation(self, sentences: str) -> str:
+        # https://colab.research.google.com/github/nateraw/stable-diffusion-videos/blob/main/stable_diffusion_videos.ipynb
+        # https://towardsdatascience.com/make-your-art-move-with-stable-diffusion-animations-80de62eec633
+        num_frames = self.num_images
+        animations = [sentences[x : x + 2] for x in range(0, len(sentences), 2)]
+        print(animations)
+        # image_path = os.path.join(self.config.output_dir, f"{id_}.png")
+        # image = self.paint(sentence)
+        # image.save(image_path)
+        # return image_path
---
+++ storyteller/pipelines/writer.py
@@ -0,0 +1,50 @@
+import logging
+import os
+from pathlib import Path
+from typing import List
+
+import nltk
+from nltk.tokenize import sent_tokenize
+import torch
+from transformers import pipeline
+
+from storyteller import StoryTellerConfig
+
+# os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+class StoryWriter:
+    def __init__(self, config: StoryTellerConfig):
+        # global model, tokenizer
+        nltk.download("punkt")
+        self.config = config
+
+        self.model = pipeline(
+            "text-generation",
+            device=torch.device(config.writer_device),
+            model=config.writer,
+            # tokenizer="gpt2",
+            # config={"max_length": 1000},
+        )
+        # self.tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+    @torch.inference_mode()
+    def _generate(self, prompt: str) -> str:
+        return self.model(
+            prompt,
+            max_new_tokens=self.config.max_new_tokens,
+            prefix=self.config.writer_prompt_prefix,
+        )[0]["generated_text"]
+
+    def generate(self, prompt: str, num_sentences: int) -> List[str]:
+        sentences = sent_tokenize(prompt)
+        while len(sentences) < num_sentences + 1:
+            prompt = self._generate(prompt)
+            sentences = sent_tokenize(prompt)
+        while len(sentences) > num_sentences + 1:
+            sentences.pop()
+        return sentences
+
+
+def init(config: StoryTellerConfig) -> StoryWriter:
+    return StoryWriter(config)
